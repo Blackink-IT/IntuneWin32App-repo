29a80ca78ef46caffe60bc914a369caaf966dc7c
atocco-BlackinkIT <97694843+atocco-BlackinkIT@users.noreply.github.com>
2023-04-09 18:07:57 -0400

    Update Uninstall.ps1
    Removed PS Transcript. When calling on the script it still outputs all needed info

--- Winget and Winget Tools/Uninstall.ps1
+++ Winget and Winget Tools/Uninstall.ps1
@@ -1,8 +1,6 @@
-#Vars for transcript
-$TranscriptPath = "C:\Windows\Temp\PS.Transcript.Uninstall.WinGet-WinGetTools.log"
+#ID of WinGet
 $ID = "Microsoft.Winget.Source_8wekyb3d8bbwe"
-#Start transcript
-Try{Start-Transcript -Path $TranscriptPath -Force -ErrorAction Stop}catch{Start-Transcript -Path $TranscriptPath -Force}
+
 try{
     Write-Host "Attempting to uninstall winget with this ID now: $ID"
     winget uninstall -h --silent --id $ID
