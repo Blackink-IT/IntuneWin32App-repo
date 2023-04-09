458b9bdb1cc4efce14f2ba59499b7f62cb7953cf
atocco-BlackinkIT <97694843+atocco-BlackinkIT@users.noreply.github.com>
2023-04-09 18:06:15 -0400

    Update Detection.ps1

--- Winget and Winget Tools/Detection.ps1
+++ Winget and Winget Tools/Detection.ps1
@@ -1,21 +1,21 @@
 #Set error action
 $ErrorActionPreference = "SilentlyContinue"
-#Transcript location
-$TranscriptPath = "C:\Windows\Temp\PS.Transcript.Detection.WinGet-WinGetTools.log"
-#Start transcript
-Try{Start-Transcript -Path $TranscriptPath -Force -ErrorAction Stop}catch{Start-Transcript -Path $TranscriptPath -Force}
+
 try{
     $Module = "WingetTools"
-    # Check if WingetTools is installed
-    if(Get-Module -Name $Module -ListAvailable){
+    # Check if WingetTools is installed and needs updated
+    # Check if WingetTools needs to be upgraded
+    $updateModule = Get-Module -Name $Module -ListAvailable
+    if($updateModule){
         Write-Host "$Module is already installed. Checking to see if it needs updated.."
-        # Check if WingetTools needs to be upgraded
-        $updateModule = Get-Module -Name $Module -ListAvailable
-        if($updateModule.Version -lt (Get-InstalledModule -Name $Module).Version){
+        if($updateModule.Version -ne (Get-InstalledModule -Name $Module).Version){
             Write-Host "$Module needs to be upgraded. Exiting with 1..."
             Stop-Transcript
             [System.Environment]::Exit(1) 
         }
+        #Import Winget module
+        Import-Module -Name $Module -Force
+        #Test if WinGet is installed and up to date
         $TestWGVersion = Test-WGVersion -Quiet
         if(($TestWGVersion) -or ($null -eq $TestWGVersion)){
             if($TestWGVersion){
@@ -24,16 +24,16 @@ try{
                 Write-Host "WinGet is not installed. Exiting with 1..."
             }
             Stop-Transcript
-            [System.Environment]::Exit(1) 
+            [System.Environment]::Exit(1)
         }else{
             Write-Host "WinGet is installed and up to date. Exiting with 0"
             Stop-Transcript
-            [System.Environment]::Exit(0) 
+            [System.Environment]::Exit(0)
         }
     }else {
         Write-Host "$Module is not installed. Exiting with 1..."
         Stop-Transcript
-        [System.Environment]::Exit(1) 
+        [System.Environment]::Exit(1)
     }
 }catch{
     [System.Environment]::Exit(1)
