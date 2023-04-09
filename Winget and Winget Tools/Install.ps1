5bef2bb95419e06c7a15ce75769f16dabb4b180a
atocco-BlackinkIT <97694843+atocco-BlackinkIT@users.noreply.github.com>
2023-04-09 18:05:50 -0400

    Update Install.ps1
    Had the install dir set incorrectly

--- Winget and Winget Tools/Install.ps1
+++ Winget and Winget Tools/Install.ps1
@@ -20,43 +20,38 @@ function DownloadJob(){
     } | Wait-Job
 }
 
-#Vars for transcript
-$TranscriptPath = "C:\Windows\Temp\PS.Transcript.Install.WinGet-WinGetTools.log"
-#Start transcript
-Try{Start-Transcript -Path $TranscriptPath -Force -ErrorAction Stop}catch{Start-Transcript -Path $TranscriptPath -Force}
 try{
     $Module = "WingetTools"
-    # Check if WingetTools is installed
-    if(Get-Module -Name $Module -ListAvailable){
+    # Check if WingetTools is installed or needs upgraded
+    $updateModule = Get-Module -Name $Module -ListAvailable
+    if($updateModule){
         Write-Host "$Module is already installed. Checking to see if it needs updated.."
-        # Check if WingetTools needs to be upgraded
-        $updateModule = Get-Module -Name $Module -ListAvailable
-        if($updateModule.Version -lt (Get-InstalledModule -Name $Module).Version){
+        if($updateModule.Version -ne (Get-InstalledModule -Name $Module).Version){
             Write-Host "$Module needs to be upgraded. Upgrading now..."
             Update-Module -Name $Module -Force
         }
     }else {
         Write-Host "$Module is not installed. Installing now..."
-        Install-Module -Name $Module -Force        
+        Install-Module -Name $Module -Force
     }
-    
+
     #Download item
-    New-Item -Path "C:\Windows\Temp\WinGet\$MSIXBundle" -ItemType Directory -Force -Confirm:$false
+    New-Item -Path "C:\Windows\Temp\WinGet" -ItemType Directory -Force -Confirm:$false
     DownloadJob -URL $URL_msixbundle -Destination "C:\Windows\Temp\WinGet\$MSIXBundle"
 
 
     #Install
     Add-AppxProvisionedPackage -Online -PackagePath "C:\Windows\Temp\WinGet\$MSIXBundle" -SkipLicense 
     Write-Host "Installation of $PackageName finished"
-    
+
     #Cleanup files
     Start-Sleep 3 # to unblock installation file
     Write-Host "Cleaning up. Deleting this folder: C:\Windows\Temp\WinGet"
     Remove-Item -Path "C:\Windows\Temp\WinGet" -Force -Recurse
-    
+
     #Stop transcript
     Stop-Transcript
-    
+
     if ( $rebootNeeded ){
         [System.Environment]::Exit(3010)
     }
