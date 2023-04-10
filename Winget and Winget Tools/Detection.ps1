75f58d1d6842ddf52bce485f29bc893273847867
atocco-BlackinkIT <97694843+atocco-BlackinkIT@users.noreply.github.com>
2023-04-10 08:55:38 -0400

    Update Detection.ps1

--- Winget and Winget Tools/Detection.ps1
+++ Winget and Winget Tools/Detection.ps1
@@ -1,10 +1,6 @@
-#Set error action
-$ErrorActionPreference = "SilentlyContinue"
-
 try{
     $Module = "WingetTools"
     # Check if WingetTools is installed and needs updated
-    # Check if WingetTools needs to be upgraded
     $updateModule = Get-Module -Name $Module -ListAvailable
     if($updateModule){
         Write-Host "$Module is already installed. Checking to see if it needs updated.."
@@ -14,20 +10,19 @@ try{
             [System.Environment]::Exit(1)
         }
         Write-Host "$Module is up to date. Importing the module..."
-        #Import Winget module
-        Import-Module -Name $Module -Force
-        #Test if WinGet is installed and up to date
-        Write-Host "$Module is imported. Testing if WinGet is missing or needs an update..."
-        $TestWGVersion = Test-WGVersion -Quiet
-        Write-Host "Test-WGVersion results: $TestWGVersion"
-        if(($TestWGVersion) -or ($null -eq $TestWGVersion)){
-            Write-Host "WinGet is either not installed or not up to date. Exiting with 1..."
-            Stop-Transcript
-            [System.Environment]::Exit(1)
-        }else{
+        #Check to see if winget exists in this directory C:\Program Files\WindowsApps
+        $Dir = dir "C:\Program Files\WindowsApps" | Where-Object{$_.Name -like "Microsoft.DesktopAppInstaller_*"}
+        $Dir = ($Dir | Where-Object{$_.Name -like "*x64*"}).FullName
+        Write-Host "This is the directory we found with a WinGet folder. Checking to see if winget.exe exists within this dir: $Dir"
+        Write-Host
+        if(Test-Path -Path "$Dir\winget.exe" -PathType Leaf){
             Write-Host "WinGet is installed and up to date. Exiting with 0"
             Stop-Transcript
             [System.Environment]::Exit(0)
+        }else{
+            Write-Host "WinGet is not installed. Exiting with 1..."
+            Stop-Transcript
+            [System.Environment]::Exit(1)
         }
     }else {
         Write-Host "$Module is not installed. Exiting with 1..."
